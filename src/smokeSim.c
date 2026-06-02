698c438445d00999fdd04475b3d5409112b5ba4e
vEmree <megaemre1@outlook.de>
2026-06-02 23:32:10 +0200

    did some stuff

--- src/smokeSim.c
+++ src/smokeSim.c
@@ -1,28 +1,121 @@
 #include <stdio.h>
-#include "raylib.h"
+#include <raylib.h>
 
-int main()
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+#define Width 5
+#define Height 3
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    SetTargetFPS(60);
-    while (!WindowShouldClose())
-    {
 
-        BeginDrawing();
+struct FluidGrid{
+    // Drei Gids erstellen um ein "MAC GRID"/Staggered Grid zu erstellen 
 
-        ClearBackground(RAYWHITE);
+    // Druck wird in der "Zellmitte" gespeichert
+    float pressure[Width * Height];
+    // X Geschwindigkeit wird an den linken und rechten Wänden 4 deswegen muss das grid eins breiter sein, wenn es 3 Zellen gibt, gibt es 4 wände von links nach rechts
+    float velocity_x[(Width + 1) * Height];
+    // Y Geschwindigkeit wird an den Wänden Oben und UNten gespeichert, deswegen muss das grid eins höher sein, wenn es 3 Zellen gibt, gibt es 4 wände von unten nach oben
+    float velocity_y[Width * (Height + 1)];
+};
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
-        EndDrawing();
+
+
+#define Cell_Size 1.0f //Temporär
+
+// FUnktion zum Errechnen der Divergenz einer Zelle
+float divergenceOfCell(struct FluidGrid *grid, int x, int y){
+
+    // Indexe für X Achse Berechnen
+    int index_left = y * (Width + 1) + x;
+    int index_right = y * (Width + 1) + (x + 1);
+    // Indexe für Y Achse Berechnen
+    int index_bottom = y * Width + x;
+    int index_top = ((y + 1) * Width) + x;
+
+    // Velocities auslesen
+    float vel_left = grid->velocity_x[index_left];
+    float vel_right = grid->velocity_x[index_right];
+    float vel_bottom = grid->velocity_y[index_bottom];
+    float vel_top = grid->velocity_y[index_top];
+
+    //Gradienten Berechen
+    float gradient_x = (vel_right - vel_left) / Cell_Size;
+    float gradient_y = (vel_top - vel_bottom) / Cell_Size;
+
+    //Gesamte Divergenz returnen
+    float div = gradient_x + gradient_y;
+    return div;
+}
+
+
+void solvePressure(struct FluidGrid *grid, int x, int y){
+    float sum_pressure = 0.0f;
+    int valid_neighbourCells = 0;
+    //Checken ob seiten Valide Cells sind zum druckausgleich
+    //Links
+    if (x - 1 >= 0){
+        sum_pressure += grid->pressure[y * Width + (x - 1)];
+        valid_neighbourCells++;
+    }
+    //Rechts
+    if (x + 1 < Width){
+        sum_pressure += grid->pressure[y * Width + (x + 1)];
+        valid_neighbourCells++;
+    }
+    //Unten
+    if (y - 1 >= 0){
+        sum_pressure += grid->pressure[(y - 1) * Width + x];
+        valid_neighbourCells++;
+    }
+    //Oben
+    if (y + 1 < Height){
+        sum_pressure += grid->pressure[(y + 1) * Width + x];
+        valid_neighbourCells++;
     }
 
-    CloseWindow();
+    float divergence = divergenceOfCell(grid, x, y);
+
+    float new_pressure = (sum_pressure - divergence) / valid_neighbourCells;
+
+    grid->pressure[y * Width + x] = new_pressure;
+
+}
+
+
+
+
+
+
+
+
+int main(){
+    // initialisieren und Clearen
+    struct FluidGrid grid;
+
+    for (int i = 0; i < Width * Height; i++){
+        grid.pressure[i] = 0.0f;
+    }
+
+    for (int i = 0; i < (Width +1) * Height; i++){
+        grid.velocity_x[i] = 0.0f;
+    }
+
+    for (int i = 0; i < Width * (Height + 1); i++){
+        grid.velocity_y[i] = 0.0f;
+    }
+
+    
+    
+
+
+
+
+
+
+
+
+    printf("bliblablub\n");
+
+
     return 0;
 }
